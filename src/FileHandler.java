fb071e6ebcb4b93d4bc6a470bc2c0c6171c9765f
RMITJake <s3905550@student.rmit.edu.au>
2023-09-24 22:44:13 +1000

    FileHandler can now read files and return content as a String

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 public class FileHandler {
+    private String path = "../records/";
     FileHandler(){
         System.out.println("Constructing FileHandler");
+    }
+
+    public void Write(String file){
+        String workingPath = path + file + ".csv";
+    }
+
+    public String Read(String file){
+        String workingPath = path + file + ".csv";
+        String fileContents = "";
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(workingPath));
+            while((line = reader.readLine()) != null)
+            {
+                fileContents += line;
+                fileContents += "\n";
+            }
+            return fileContents;
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return "";
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -4,5 +4,6 @@ public class Menu {
 
         // Create the FileHandler
         FileHandler file = new FileHandler();
+        System.out.println(file.Read("Login"));
     }
 }
