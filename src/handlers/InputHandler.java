2bf6d1840521282a1b45d1e0f33f4f8324ed7d67
RMITJake <s3905550@student.rmit.edu.au>
2023-09-29 22:31:42 +1000

    Disabled java.io.Console as it is causing exceptions with Eclipse

--- src/handlers/InputHandler.java
+++ src/handlers/InputHandler.java
@@ -1,13 +1,14 @@
 package src.handlers;
 import java.util.Scanner;
-import java.io.Console;
+// import java.io.Console;
 public class InputHandler {
     // Create the Scanner to actually take stdin
     Scanner scn = new Scanner(System.in);
     String input;
 
+    // DISABLED CONSOLE AS CAUSING CRASH WITH ECLIPSE JDK
     // Create the Console object to obfuscate input for passwords
-    Console console = System.console();
+    // Console console = System.console();
 
     // General getInput function
     public String getInput(){
@@ -17,7 +18,8 @@ public class InputHandler {
 
     // Get input hide the input from the UI
     public String getSecureInput(){
-        input = String.valueOf(console.readPassword());
+        // input = String.valueOf(console.readPassword());
+        input = scn.nextLine();
         return input;
     }
 }
