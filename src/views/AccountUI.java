b956d6fbf498ef4e44d1dc63c374f1b27715ed70
s3766070 <91061313+s3766070@users.noreply.github.com>
2023-10-14 15:52:16 +0900

    Update AccountUI.java

--- src/views/AccountUI.java
+++ src/views/AccountUI.java
@@ -14,7 +14,7 @@ public class AccountUI {
   }
 
   public void password(){
-    System.out.println("Enter a new password (Password must have a lower case letter, a uppercase letter and one number): ");
+    System.out.println("Enter a new password (Password must have a lower case letter, a uppercase letter, one number and minimum 20 characters): ");
   }
 
   public void passwordConfirm(){
