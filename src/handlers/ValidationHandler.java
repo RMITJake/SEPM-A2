3ea85f21161941a974e593e4209857131f6d2e85
seancarbondioxide <S3947968@student.rmit.edu.au>
2023-10-14 13:41:58 +1100

    Update password length requirements to 20 when creating account

--- src/handlers/ValidationHandler.java
+++ src/handlers/ValidationHandler.java
@@ -69,7 +69,7 @@ public class ValidationHandler {
     // Per spec we check for uppercase and lowercase alphanumeric, no symbols
     // TEMPORARY - ONLY CHECKING FOR 4 MIN CHARACTERS FOR TESTING, PER SPEC CHECK FOR 20
     // Check length of the string
-    regex = "^[a-zA-Z\\d]{4,}";
+    regex = "^[a-zA-Z\\d]{20,}";
     lengthMatch = Pattern.matches(regex, input);
 
     // Check uppercase
